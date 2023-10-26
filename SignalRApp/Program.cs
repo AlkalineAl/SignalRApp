1f05c59572fae1aab63f51491aa0d7a08a83bd8f
Алексей Поддубный <alkalineal54@gmail.com>
2023-10-26 19:47:55 +0300

    Добавьте файлы проекта.

---
+++ SignalRApp/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Http.Connections; // пространство имен перечисления HttpTransportType
+
+using SignalRApp;   // пространство имен класса ChatHub
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat",
+    options => {
+        options.ApplicationMaxBufferSize = 128;
+        options.TransportMaxBufferSize = 128;
+        options.LongPolling.PollTimeout = TimeSpan.FromMinutes(1);
+        options.Transports = HttpTransportType.LongPolling | HttpTransportType.WebSockets;
+    });
+app.Run();
---
+++ SignalrWpfClient/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+using System;
+using System.Windows;
+
+namespace SignalrWpfClient
+{
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;  // подключение для взаимодействия с хабом
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // создаем подключение к хабу
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7013/chat")
+                .Build();
+
+
+            // регистрируем функцию Receive для получения данных
+            connection.On<string, string>("Receive", (user, message) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    chatbox.Items.Insert(0, newMessage);
+                });
+            });
+        }
+
+        // обработчик загрузки окна
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // подключемся к хабу
+                await connection.StartAsync();
+                chatbox.Items.Add("Вы вошли в чат");
+                sendBtn.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                chatbox.Items.Add(ex.Message);
+            }
+        }
+        // обработчик нажатия на кнопку
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // отправка сообщения
+                await connection.InvokeAsync("Send", userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                chatbox.Items.Add(ex.Message);
+            }
+        }
+
+
+        private async void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            await connection.InvokeAsync("Send", "", $"Пользователь {userTextBox.Text} выходит из чата");
+            await connection.StopAsync();   // отключение от хаба
+        }
+    }
+}
